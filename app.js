1ef6f745731292c326cae699936bde8ba562d5f9
Lam Thi Sa Phuol <lamthisaphuol@gmail.com>
2025-11-01 10:23:28 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -11,4 +12,5 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 
+app.use("/api/contacts", contactRouter);
 module.exports = app;
